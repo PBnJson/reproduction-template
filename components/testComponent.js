67139fe975d739babaccd490d73a4e1a7e8e8f1e
J Clancy <ukg_123@yahoo.com>
2024-08-24 15:29:42 -0500

    testing sw, changing testComp

--- components/testComponent.js
+++ components/testComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function testComponent() {
     return (
         <div>
-            <h1>THIS IS A TEST</h1>
+            <h1><p>Testing sw change take 1</p></h1>
         </div>
     )
 }
--- pages/testComponent/index.js
+++ pages/testComponent/index.js
@@ -1,4 +1,4 @@
-import TestComponent from '../components/testComponent';
+import TestComponent from '../../components/testComponent';
 
 export default function Home() {
     return (
