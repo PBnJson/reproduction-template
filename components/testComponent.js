51e40feb1b31dbe5259795ac2c970bfe013c9e92
J Clancy <ukg_123@yahoo.com>
2024-08-24 15:16:31 -0500

    testing sw change, testComponent unchanged

---
+++ components/testComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function testComponent() {
+    return (
+        <div>
+            <h1>THIS IS A TEST</h1>
+        </div>
+    )
+}
---
+++ pages/testComponent/index.js
@@ -0,0 +1,10 @@
+import TestComponent from '../components/testComponent';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <TestComponent />
+        </div>
+    );
+}
