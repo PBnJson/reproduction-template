88569ac688de995c6cfc5107dc56d12bd6c34067
J Clancy <ukg_123@yahoo.com>
2024-08-24 15:07:59 -0500

    first commit to reproduce error

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+    // For use with generating Next PWA
+    experimental: {
+        newNextLinkBehavior: true,
+    },
+    // For use with Vercel Blob
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'my-blob-store.public.blob.vercel-storage.com',
+                port: '',
+            },
+        ],
+    },
+    // swcMinify: false,
+});
+
+module.exports = nextConfig;
